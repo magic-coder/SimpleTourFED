2e45b31a148e3a2c8b37c10160057e184b9221fc
ashbringerL <ll596374@vip.qq.com>
2016-05-18 11:00:30 +0800

    merge alert.js && sidebar.js

--- common/js/base.js
+++ common/js/base.js
@@ -1,3 +1,37 @@
+/**
+ * Created by ashbringer on 16/5/18.
+ */
+
+
+//sidebar.js
+(function($){
+    $.fn.sidebar = function(){
+        if($('.sidebar').length == 0){
+            $('<div class="sidebar"><div class="sidebar-content"><div class="sidebar-title"><div class="left"></div><div class="right">13023451234</div></div>'
+                +'<a href="#"><div class="sidebar-item"><div class="left sidebar-index"></div><div class="right">首页</div></div></a>'
+                +'<a href="#"><div class="sidebar-item"><div class="left sidebar-order"></div><div class="right">我的订单</div></div></a>'
+                +'<a href="#"><div class="sidebar-item"><div class="left sidebar-tourism"></div><div class="right">我的行程</div></div></a>'
+                +'<a href="#"><div class="sidebar-item"><div class="left sidebar-admin"></div><div class="right">个人中心</div></div></a>'
+                +'</div></div>').appendTo('body');
+        }
+        $(this).click(function(){
+            $('<div class="sidebar-backdrop"></div>').appendTo('body');
+            setTimeout(function(){
+                $('.sidebar-backdrop').addClass('in');
+                $('.sidebar').addClass('sidebar-in sidebar-transition');
+            },10)
+            $('.sidebar-backdrop').click(function(){
+                $('.sidebar-backdrop').removeClass("in");
+                $('.sidebar').removeClass('sidebar-in');
+                setTimeout(function(){
+                    $('.sidebar-backdrop').remove();
+                },150)
+            })
+        })
+    }
+})(Zepto);
+
+//alert.js
 (function($){
     function Modal(option,type){
         var _ = this;
